c3788198dded24755b26820ce2aec13293640cd2
lcokedops <leonardodias97@gmail.com>
2025-01-22 00:47:29 -0300

    Seprando por sexo

--- src/Programa/TarefaColecoes.java
+++ src/Programa/TarefaColecoes.java
@@ -11,24 +11,35 @@ public class TarefaColecoes {
 		Scanner sc = new Scanner(System.in);
 		
 		String dadosPessoas;
+		List<Pessoa> feminino = new ArrayList<>();
+		List<Pessoa> masculino = new ArrayList<>();
 		
 		System.out.println("Digite o nome e o sexo da pessoa, sendo o nome e o sexo separado por - e cada pessoa separada por vírgula: ");
 		dadosPessoas = sc.nextLine();
 		
+		//Separar os dados de cada pessoa
 		String[] nomesSeparados = dadosPessoas.split("[,]");
-		List<Pessoa> listaPessoas = new ArrayList<>();
-		
+				
+		//Separar o nome e o sexo
 		for (String s : nomesSeparados) {
 			String[] dados = s.split("[-]");
-			listaPessoas.add(new Pessoa(dados[0], dados[1]));
+			
+			//Separar pessoa pelo sexo
+			if (dados[1].equals("Feminino")) {
+				feminino.add(new Pessoa(dados[0], dados[1]));
+			} else {
+				masculino.add(new Pessoa(dados[0], dados[1]));
+			}
+			
 		}
 		
-		//Ordenar a lista
-		Collections.sort(listaPessoas);
+		//Ordenar as listas
+		Collections.sort(feminino);
+		Collections.sort(masculino);
 		
-		System.out.println("\nLista de pessoas ordenada:");
-		System.out.println(listaPessoas);
-		
+		//Imprimir as duas lista ordenadas e separadas
+		System.out.println("\nLista de mulheres:\n" + feminino);
+		System.out.println("\nLista de homens:\n" + masculino);
 	}
 
 }
